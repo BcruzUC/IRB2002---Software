333857e781496f3571ca7cec7f5dc3cd36649e5d
BcruzUC <bcruz@uc.cl>
2018-10-13 10:18:31 -0300

    Actualizando Machine y Measures: uso de bases de datos internet

--- Machine.py
+++ Machine.py
@@ -5,22 +5,49 @@ from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from time import sleep
 import numpy as np
+import random as rn
 import serial
 import os
 
 
+def get_both_database(data1, data2, _size=300):
+    channel_1 = data1[:, 1:]
+    channel_2 = data2[:, 1:]
+    output = np.array([])
+    for i in range(6):
+        temp1 = channel_1[i*_size:(i * _size) + _size, :]
+        temp2 = channel_2[i*_size:(i * _size) + _size, :]
+        label = np.ones((2 * _size, 1)) * i
+        temp_out = np.concatenate((temp1, temp2), axis=0)
+        temp_out = np.concatenate((label, temp_out), axis=1)
+        if not output.any():
+            output = temp_out
+        else:
+            output = np.concatenate((output, temp_out), axis=0)
+        np.save('Database_2_total', output)
 
+def normalizar(data_file):
+    mean = data_file.mean(axis=0)
+    std = data_file.std(axis=0)
+    return (data_file - mean) / std
 
 
 def extract_features(mode, ncmp):
-    feats = ''
-    labels = ''
-    if 'features_{}.npy'.format(mode) in os.listdir("./"):
+    feats = None
+    labels = None
+    names = None
+    if f'{mode}.npy' or f"features_{mode}.npy" in os.listdir("./"):
 
         # # Para futuro.. por ahora simple nomas
         # load = input("[Seleccion] Se han detectado archivos '.npy'. Deseas cargarlos para ahorrar tiempo? y/n: ")
+        if not 'Database' in mode:
+            mode = f"features_{mode}"
+        data = np.load(mode + '.npy')
+        if 'ch1' in mode:
+            names = np.load('Database_2_names_ch1.npy')
+        if 'ch2' in mode:
+            names = np.load('Database_2_names_ch2.npy')
 
-        data = np.load(f"features_{mode}.npy")
         labels, feats = np.array(data[:, :1]).astype(int),\
                         np.array(data[:, 1:]).astype(int)
 
@@ -35,7 +62,7 @@ def extract_features(mode, ncmp):
             pca.fit(feats)
             feats = pca.transform(feats)
 
-            return feats, labels, None, pca
+            return feats, labels, None, pca, names
 ##        save = input("Deseas guardar los resultados para futuras instancias? y/n: \n")
 ##        if save.lower() == 'y':
 ##            np.save("{}feats_data".format(mode+"_"), feats)
@@ -45,11 +72,9 @@ def extract_features(mode, ncmp):
         #Extraer nuevas features.. para futuro tambien
         pass
 
-    return feats, labels, None, None
+    return feats, labels, None, None, names
 
-
-
-def classify(feats_dict, Test_data, ncmp):
+def train_predict(feats_dict, ncmp):
     print("[Clasificacion] Entrenando clasificadores...")
 
     feats, labels = feats_dict["feats"], feats_dict["labels"]
@@ -64,18 +89,23 @@ def classify(feats_dict, Test_data, ncmp):
     knn = KNN(n_neighbors=5)
     knn.fit(feats, labels)
 
+    return lda, svm, knn
+
+def classify(Test_data, names, lda, svm, knn):
+
     print("[Clasificacion] Probando clasificadores...")
 
     lda_predict = lda.predict(Test_data)[0]
     svm_predict = svm.predict(Test_data)[0]
     knn_predict = knn.predict(Test_data)[0]
 
-    print(f"LDA: {lda_predict} || SVM:  {svm_predict} || KNN: {knn_predict}")
+    print(f"LDA: {names[lda_predict]} || SVM:  {names[svm_predict]} ||"
+          f" KNN: {names[knn_predict]}")
     print()
 
     return lda
 
-def get_measure():
+def get_measure(length):
     ser = serial.Serial("COM4", baudrate=115200, timeout=1)
     print('Tomando medida en 3..')
     sleep(0.5)
@@ -86,29 +116,51 @@ def get_measure():
     print('¡ Ya !')
     sleep(0.5)
     feature_line = []
-    while len(feature_line) < 5000:
+    while len(feature_line) < length:
         line = ser.readline()
         data = str(line.decode('cp437')).strip('\r0\r\n')
         if not data.isnumeric():
             data = 0
         feature_line.append(data)
-    return np.array(feature_line).astype(int)
+    final_data = np.array(feature_line).astype(int)
+    return normalizar(final_data)
     print()
+
+def create_measure(_type, peak_val, duration, length=5000):
+    measure = np.zeros((1, length))
+    if _type == 'finger':
+        measure[:, 1000: 1000 + duration] = np.ones((1, duration)) * \
+                                            peak_val * rn.uniform(0.6, 1.2)
+    if _type == 'fist':
+        measure[:, 100:] = np.ones((1, length - 100)) * peak_val * \
+                           rn.uniform(0.6, 1.2)
+    return measure
 
 
 if __name__ == '__main__':
+
+    data1 = np.load('Database_2_total_ch1.npy')
+    data2 = np.load('Database_2_total_ch2.npy')
+    get_both_database(data1, data2, _size=300)
+
+    input()
+
     """Funcion principal del programa"""
-    ncmp = 5000
+
+    ncmp = 2500
+
     # Training Feature Extraction
     print("\n[Extraccion Features] Procesando datos...")
     file = input('Ingrese sector de los datos: ')
-    feats, labels, scaler, train_pca = extract_features(file, ncmp=ncmp)
+    feats, labels, scaler, train_pca, names = extract_features(file, ncmp=ncmp)
 
     feats_dict = {'feats': feats, 'labels': labels}
-
+    trained_pred = train_predict(feats_dict, ncmp=ncmp)
     continuar = True
     while continuar:
-        test_line = get_measure()
+        test_line = get_measure(length=2500)
+        # option = input('Choose finger / fist: ')
+        # test_line = create_measure(option, peak_val=1, duration=300, length=2500)
         if scaler or train_pca:
             print('Aplicando PCA a test line')
             _size = feats_dict["feats"].shape[0]
@@ -117,7 +169,7 @@ if __name__ == '__main__':
             test_line = train_pca.transform(test_line)
             test_line = test_line[:1, :]
         test_line = test_line.reshape(1, -1)
-        classify(feats_dict, test_line, ncmp=ncmp)
+        classify(test_line, names, *trained_pred)
 
         continuar = True if input('Desea continuar? y/n: ') == 'y' else False
 
--- Measures.py
+++ Measures.py
@@ -3,36 +3,45 @@ import numpy as np
 import serial
 from scipy.fftpack import fft
 import matplotlib.pyplot as plt
+from scipy import signal
+import scipy as sp
+from collections import defaultdict
+import scipy.io as sio
+import os
 
-write_to_file_path = "output.txt"
-ser = serial.Serial("COM4", baudrate=115200, timeout=1)
 
 
-#Crear list de datos que lueego pase a graficarse..
+# write_to_file_path = "output.txt"
+# ser = serial.Serial("COM4", baudrate=115200, timeout=1)
+#
+#
+# #Crear list de datos que lueego pase a graficarse..
+#
+#
+# while True:
+#     list_data = []
+#     while len(list_data) < 10000:
+#         line = ser.readline()
+#         data = str(line.decode('cp437')).strip('\r0\r\n')
+#         if not data.isnumeric():
+#             data = 0
+#         print(data)
+#         list_data.append(int(data))
+#
+#     print(list_data)
+#     list_data = np.array(list_data)
+#
+#     N = len(list_data)
+#     T = 1.0 / 800.0
+#
+#     yf = fft(list_data)
+#     xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+#     plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+#     plt.grid()
+#     plt.show()
+#     plt.close()
 
-
-while True:
-    list_data = []
-    while len(list_data) < 10000:
-        line = ser.readline()
-        data = str(line.decode('cp437')).strip('\r0\r\n')
-        if not data.isnumeric():
-            data = 0
-        print(data)
-        list_data.append(int(data))
-
-    print(list_data)
-    list_data = np.array(list_data)
-
-    N = len(list_data)
-    T = 1.0 / 800.0
-
-    yf = fft(list_data)
-    xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
-    plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
-    plt.grid()
-    plt.show()
-    plt.close()
+#### Archivo original ! ###
 
 # output_file = open(write_to_file_path, "w");
 # while True:
@@ -54,3 +63,50 @@ while True:
 # plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
 # plt.grid()
 # plt.show()
+
+## CHANGE DATABASE 1 & 2 TO TOTAL FORM
+
+
+def sort_measures(data, channel='ch1'):
+    data_keys = [name for name in list(data) if channel in name]
+    total_data = np.zeros((300, 2501))
+    for i, name in enumerate(data_keys):
+        y, x = data[name].shape
+        label = np.ones((y, 1)) * i
+        temp_data = np.concatenate((label, data[name]), axis=1)
+        if not total_data.any():
+            total_data[:, :] = temp_data
+        else:
+            total_data = np.concatenate((total_data, temp_data), axis=0)
+    return total_data, np.array(data_keys)
+
+
+temp_dict = defaultdict(list)
+folder_name = 'Database_2'
+
+dir_list = os.listdir(folder_name)
+for path in dir_list:
+    data = sio.loadmat('{}/{}'.format(folder_name, path))
+    dict_keys = list(data)[3:]
+    for _key in dict_keys:
+        _array = np.array(data[_key])
+        temp_dict[_key].append(_array)
+
+
+
+total_dict = {}
+for ind in list(temp_dict):
+    test = np.array(temp_dict[ind])
+    conct = np.concatenate((test[0, :, :], test[1, :, :], test[2, :, :]), axis=0)
+    if folder_name == 'Database_1':
+        conct = np.concatenate((test[0, :, :], test[1, :, :], test[2, :, :],
+                            test[3, :, :], test[4, :, :]), axis=0)
+    total_dict[ind] = conct
+
+channel = input('Elija set de datos [ch1/ ch2]: ')
+save_data, data_keys = sort_measures(total_dict, channel=channel)
+save_data = abs(save_data)
+
+# np.save('{}_total_{}'.format(folder_name, channel), save_data)
+np.save('{}_names_{}'.format(folder_name, channel), data_keys)
+
